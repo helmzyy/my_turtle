ae458876f6d746e2c40614c3bb4691a488b9edfe
helmzyy <AHELMS9995@GMAIL.COM>
2016-12-18 23:09:50 -0500

    Add files via upload

--- my_turtle.py
+++ my_turtle.py
@@ -1,68 +1,68 @@
 import turtle
 
 
-my_turtle = turtle.Turtle()
-my_turtle.speed(0)
+arrow = turtle.Turtle()
+arrow.speed(0)
 
 def square():
     for lines in range(3):
-        my_turtle.forward(100)
+        arrow.forward(100)
 
-        my_turtle.left(90)
+        arrow.left(90)
 
     for lines in range(4):
 
-        my_turtle.forward(100)
+        arrow.forward(100)
 
-        my_turtle.right(90)
+        arrow.right(90)
     
    
-    my_turtle.forward(200)
+    arrow.forward(200)
 
     for lines in range(2):
 
-        my_turtle.right(90)
+        arrow.right(90)
 
-        my_turtle.forward(100)
+        arrow.forward(100)
 
-    my_turtle.right(90)
+    arrow.right(90)
 
-    my_turtle.forward(200)
+    arrow.forward(200)
 
     for lines in range(2):
 
-        my_turtle.right(90)
+        arrow.right(90)
 
-        my_turtle.forward(100)
+        arrow.forward(100)
 
 
 def diamond():
 
-    my_turtle.right(45)
+    arrow.right(45)
 
     for lines in range(3):
 
-        my_turtle.forward(141)
+        arrow.forward(141)
 
-        my_turtle.right(90)
+        arrow.right(90)
 
-    my_turtle.forward(141)
+    arrow.forward(141)
 
 
 def one():
     square()
     diamond()
-    my_turtle.left(225)
-    my_turtle.forward(100)
+    arrow.left(225)
+    arrow.forward(100)
  
 
 def octagon(): 
     for octs in range(7):
-      my_turtle.forward(100)
-      my_turtle.left(45) 
+      arrow.forward(100)
+      arrow.left(45) 
    
-    my_turtle.forward(100)
-    my_turtle.right(45/2)  
+    arrow.forward(100)
+    arrow.right(45/2)  
 
 
 for octogon in range(16):
